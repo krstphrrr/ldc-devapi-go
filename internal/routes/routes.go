6c4423457a992f18e56b62b123561d1555c5efb7
kris <kristopher.bonefont@gmail.com>
2025-01-05 20:24:43 -0700

    added middleware to the dynamic router

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -3,14 +3,26 @@ package routes
 import (
 	"database/sql"
 	"net/http"
+	
+    "go-api-app/internal/middleware"
 	"go-api-app/internal/services"
 	"go-api-app/config"
 )
 
 func RegisterRoutes(mux *http.ServeMux, db *sql.DB) {
 	for endpoint := range config.EndpointToTableMap {
-		mux.HandleFunc(endpoint, func(w http.ResponseWriter, r *http.Request) {
+		// Handling endpoints that don't require middleware 
+
+		// if endpoint == "/tblProject" {
+		// 	mux.Handle(endpoint, http.HandlerFunc(services.tblProjectHandler))
+		
+		// } else {
+		// 	mux.Handle(endpoint, middleware.AuthMiddleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// 		services.GenericDynamicDataHandler(w, r, db)
+		// 	})))
+		// }
+		mux.Handle(endpoint, middleware.AuthMiddleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			services.GenericDynamicDataHandler(w, r, db)
-		})
+		})))
 	}
 }
