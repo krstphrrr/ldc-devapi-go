5a149662b6d898367a87aca1149301203e1cccf6
Kris! <kristopher.bonefont@gmail.com>
2025-05-15 15:23:29 -0600

    switched to public_dev

--- config/config.go
+++ config/config.go
@@ -36,19 +36,19 @@ type AppConfig struct {
 
 
 var EndpointToTableMap = map[string]string{
-	"/dataLPI":     "public_test.datalpi_filtered_view",
-	"/dataHeader": "public_test.dataheader_filtered_view",
-	"/dataHeight": "public_test.dataheight_filtered_view",
-	"/dataGap": "public_test.datagap_filtered_view",
-	"/dataHorizontalFlux": "public_test.datahorizontalflux_filtered_view",
-	"/dataPlotCharacterization": "public_test.dataplotcharacterization_filtered_view",
-	"/dataSoilHorizons": "public_test.datasoilhorizons_filtered_view",
-	"/dataSoilStability": "public_test.datasoilstability_filtered_view",
-	"/dataSpeciesInventory": "public_test.dataspeciesinventory_filtered_view",
-	"/geoIndicators": "public_test.geoindicators_filtered_view",
-	"/geoSpecies": "public_test.geospecies_filtered_view",
-	"/tblRHEM": "public_test.tblrhem_filtered_view",
-	"/tblProject": "public_test.tblproject_filtered_view",
+	"/dataLPI":     "public_dev.datalpi_filtered_view",
+	"/dataHeader": "public_dev.dataheader_filtered_view",
+	"/dataHeight": "public_dev.dataheight_filtered_view",
+	"/dataGap": "public_dev.datagap_filtered_view",
+	"/dataHorizontalFlux": "public_dev.datahorizontalflux_filtered_view",
+	"/dataPlotCharacterization": "public_dev.dataplotcharacterization_filtered_view",
+	"/dataSoilHorizons": "public_dev.datasoilhorizons_filtered_view",
+	"/dataSoilStability": "public_dev.datasoilstability_filtered_view",
+	"/dataSpeciesInventory": "public_dev.dataspeciesinventory_filtered_view",
+	"/geoIndicators": "public_dev.geoindicators_filtered_view",
+	"/geoSpecies": "public_dev.geospecies_filtered_view",
+	"/tblRHEM": "public_dev.tblrhem_filtered_view",
+	"/tblProject": "public_dev.tblproject_filtered_view",
 	"/dataAeroSummary": "aero_data.aero_summary",
 }
 
